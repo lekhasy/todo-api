a61f684945f7940c21f5e74a3aff528f47ed9a27
sy1lk <sy1lk@vn.ibm.com>
2021-01-15 10:34:01 +0700

    add swagger even in publish mode

--- Startup.cs
+++ Startup.cs
@@ -37,12 +37,12 @@ namespace todo_api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "todo_api v1"));
-            }
+            // if (env.IsDevelopment())
+            // {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "todo_api v1"));
+            // }
 
             app.UseCors();
 
